02a01573cdccd3358753d8d3889a0b60098a0749
aditya-ai00 <adityanyo39@gmail.com>
2026-04-19 22:25:53 +0530

    Complete weather dashboard

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const apiKey = "f0133e94263d448c963164120261904";
 
+
 function getWeather() {
 
 const city = document.getElementById("city").value.trim();
@@ -10,7 +11,7 @@ return;
 }
 
 const url =
-`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(city)}`;
 
 fetch(url)
 .then(res => res.json())
@@ -20,6 +21,19 @@ if (data.error) {
 alert(data.error.message);
 return;
 }
+
+updateWeather(data);
+
+})
+.catch(() => {
+alert("Failed to fetch weather data");
+});
+
+}
+
+
+
+function updateWeather(data){
 
 document.getElementById("name").innerText =
 data.location.name + ", " + data.location.country;
@@ -33,5 +47,36 @@ data.current.condition.text;
 document.getElementById("icon").src =
 "https:" + data.current.condition.icon;
 
+}
+
+
+
+document.getElementById("city").addEventListener("keypress", function(e){
+
+if(e.key === "Enter"){
+getWeather();
+}
+
 });
+
+
+
+navigator.geolocation.getCurrentPosition(showPosition);
+
+function showPosition(position){
+
+const lat = position.coords.latitude;
+const lon = position.coords.longitude;
+
+const url =
+`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`;
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+
+updateWeather(data);
+
+});
+
 }
